cc4ed385f97c59ed37c2debb5f68cc16fed62a11
Angad <cro@optipheonix.com>
2021-06-30 11:54:02 +0530

    update: started with python

---
+++ python/basics.py
@@ -0,0 +1,15 @@
+from math import *
+print('hello')
+# string template
+
+# variables and data types (it will take value you assign to variable name)
+angad_raj = 'giraffe Academy';
+print(angad_raj[0]);
+
+# numbers
+my_num = 20.20;
+print(ceil(my_num));
+
+# taking input
+my_input = input('enter name');
+print(my_input)
