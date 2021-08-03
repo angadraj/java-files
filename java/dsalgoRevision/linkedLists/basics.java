8704ef247920882010a21a4998f91f33a80de876
Angad <cro@optipheonix.com>
2021-08-04 02:05:02 +0530

    completed till linkedlists lvl1 and lvl 2

---
+++ java/dsalgoRevision/linkedLists/basics.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class basics {
+    private class Node {
+        int val;
+        Node next;
+
+        Node (int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class LinkedList {
+        int size;
+        Node head;
+        Node tail;
+
+        // all the functions are defined here
+    }
+
+    public void solve() {
+
+    }
+
+    public static void main(String args[]) {
+        solve();
+    }
+}
