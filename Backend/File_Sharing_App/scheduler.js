6eb7a0138878afc085e49056c9d75fea9a3cb99c
Angad <cro@optipheonix.com>
2022-04-05 22:47:24 +0530

    added till file sharing app

--- Backend/File_Sharing_App/scheduler.js
+++ Backend/File_Sharing_App/scheduler.js
@@ -1,15 +1,25 @@
 const fileModel = require('./models/fileModel');
+const fs = require('fs');
+const db = require('./dataBase/db');
+
 async function cleanUp() {
     try {
         let pastDate = new Date(Date.now() - (24 * 60 * 60 * 1000));
         let files = await fileModel.find({createdAt: {$lt: pastDate}});
         if (files.length) {
             for (let file of files) {
-                
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log("successfully deleted");
             }
         }
-
     } catch (e) {
         console.log(e);
     }
 }
+
+cleanUp()
+.then(function() {
+    console.log("files deleted!");
+    process.exit();
+})
